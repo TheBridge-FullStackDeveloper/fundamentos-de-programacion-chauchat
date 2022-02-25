71c5a63b3c245fc9411a8ca5a83edb656dd2a0c2
Brujitari <natimontesg2@mail.com>
2022-02-25 20:16:11 +0100

    feat: ahora se atasca un 80 % de las veces

--- Chauchau.js
+++ Chauchau.js
@@ -1,2 +1,17 @@
-const cargador = ["pium!", "pium!", "pium!", "pium!", "pium!", "pium!", "pium!" ]
+const cargador = ['Pium!', 'Pium!', 'Pium!', 'Pium!', 'Pium!', 'Pium!', 'Pium!']
 
+const numeroEntre0y5 = Math.random()*5
+
+function chauchat (array) {
+
+  if (numeroEntre0y5 >= 1) {
+
+  for(let i = 0; i < array.length; i++) {
+  console.log(array[i])
+  } 
+  
+} else {
+    console.log('No va')
+  } 
+}
+chauchat(cargador)
