0a4b85e23a5f5ee8df556d98805ce04ea28bca45
Brujitari <natimontesg2@mail.com>
2022-02-25 21:09:49 +0100

    feat: added some comments

--- Chauchau.js
+++ Chauchau.js
@@ -1,22 +1,27 @@
 const cargador = ['Pium!', 'Pium!', 'Pium!', 'Pium!', 'Pium!', 'Pium!', 'Pium!']
 
-const numeroEntre0y5 = Math.random() * 5
+const numeroEntre0y5 = Math.random() * 5   //random number [0 to 5)
 
 function chauchat(array) {
 
     if (numeroEntre0y5 >= 1) {
 
         for (let i = 0; i < array.length; i++) {
-            console.log(array[i])
 
             if (i % 3 === 0) {
                 console.log('')
             }
 
+
+            console.log(array[i])
+
+            
         }
 
     } else {
         console.log('No va')
     }
 }
+
+
 chauchat(cargador)
